2133a43c1c7be841efc8f3760b00be105ac5b732
Hemanth Kumar <pns7371@gmail.com>
2022-09-04 18:14:10 +0530

    project initial commit

---
+++ src/main/java/com/example/SpringBootGitPractice/SpringBootGitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootGitPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitPracticeApplication.class, args);
+	}
+
+}
